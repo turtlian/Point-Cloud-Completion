e08fa1d4eb12043f727816c7903df93decbdd97e
2yejin2 <62421163+2yejin2@users.noreply.github.com>
2021-05-29 23:26:10 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ import transforms3d
 import random
 import math
 from matplotlib import pyplot as plt
+from collections import Iterable
 
 # data
 def load_h5_file(path):
@@ -20,7 +21,7 @@ def augmentation(partial, target, scale, rotation, mirror_prob):
     transform_matrix = transforms3d.zooms.zfdir2mat(1)
 
     if scale is not None:
-        scaling_num = random.uniform(1/scale, scale)
+        scaling_num = scale
         transform_matrix = np.dot(transforms3d.zooms.zfdir2mat(scaling_num), transform_matrix)
     if rotation:
         angle = random.uniform(0, 2*math.pi)
@@ -85,3 +86,92 @@ def plot_pcds(pcds, titles, use_color=[],color=None, suptitle='', sizes=None, cm
         plt.close(fig)
     else:
         plt.show()
+
+
+# logging
+class AverageMeter(object):
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.sum_2 = 0  # sum of squares
+        self.count = 0
+        self.std = 0
+
+    def update(self, val, n=1):
+        if val != None:  # update if val is not None
+            self.val = val
+            self.sum += val * n
+            self.sum_2 += val ** 2 * n
+            self.count += n
+            self.avg = self.sum / self.count
+            self.std = np.sqrt(self.sum_2 / self.count - self.avg ** 2)
+        else:
+            pass
+
+
+class Logger(object):
+    def __init__(self, path, int_form=':03d', float_form=':.4f'):
+        self.path = path
+        self.int_form = int_form
+        self.float_form = float_form
+        self.width = 0
+
+    def __len__(self):
+        try:
+            return len(self.read())
+        except:
+            return 0
+
+    def write(self, values):
+        if not isinstance(values, Iterable):
+            values = [values]
+        if self.width == 0:
+            self.width = len(values)
+        assert self.width == len(values), 'Inconsistent number of items.'
+        line = ''
+        for v in values:
+            if isinstance(v, int):
+                line += '{{{}}} '.format(self.int_form).format(v)
+            elif isinstance(v, float):
+                line += '{{{}}} '.format(self.float_form).format(v)
+            elif isinstance(v, str):
+                line += '{} '.format(v)
+            else:
+                raise Exception('Not supported type.', v)
+        with open(self.path, 'a') as f:
+            f.write(line[:-1] + '\n')
+
+    def read(self):
+        with open(self.path, 'r') as f:
+            log = []
+            for line in f:
+                values = []
+                for v in line.split(' '):
+                    try:
+                        v = float(v)
+                    except:
+                        pass
+                    values.append(v)
+                log.append(values)
+        return log
+
+def draw_curve(work_dir, train_logger, test_logger):
+        train_logger = train_logger.read()
+        test_logger = test_logger.read()
+        epoch, train_loss = zip(*train_logger)
+        epoch,test_loss = zip(*test_logger)
+
+        plt.plot(epoch, train_loss, color='blue', label="Train Loss")
+        plt.plot(epoch, test_loss, color='red', label="Test Loss")
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
+        plt.title("Loss Curve")
+        plt.legend()
+        plt.grid(True)
+        plt.savefig(work_dir + '/loss_curve.png')
+        plt.close()
