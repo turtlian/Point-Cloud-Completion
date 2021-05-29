44ef3a95ad32746956a3c93a90c7964b984e4cb7
2yejin2 <62421163+2yejin2@users.noreply.github.com>
2021-05-29 23:25:24 +0900

    Update model.py

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ tree_arch = [2, 4, 4, 4, 4, 4]
 mlp_dim = [256, 64, 8]
 
 class TopNet(nn.Module):
-    def __init__(self, encoder_feature_dim):
+    def __init__(self, encoder_feature_dim, device):
         super(TopNet, self).__init__()
         self.decoder_feature_dim = mlp_dim[-1]
 
@@ -18,7 +18,7 @@ class TopNet(nn.Module):
         # decoder: root node
         self.mlp = decoder_mlp(encoder_feature_dim, self.decoder_feature_dim, mlp_dim, tree_arch[0])
         # decoder: leaf node
-        self.mlpconv_layers = decoder_mlp_conv(encoder_feature_dim, self.decoder_feature_dim, tree_arch)
+        self.mlpconv_layers = decoder_mlp_conv(encoder_feature_dim, self.decoder_feature_dim, tree_arch, device)
 
     def forward(self, x):
         feature = self.pcn_encoder(x)
