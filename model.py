a01841726dbdce66fe0156d7527da26cabc9ee30
2yejin2 <62421163+2yejin2@users.noreply.github.com>
2021-05-30 21:34:58 +0900

    Update model.py

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from net_utils import PCNencoder, decoder_mlp, decoder_mlp_conv
+from net_utils import PCNencoder, decoder_mlp, decoder_mlp_conv, get_arch
 import torch.nn.functional as F
 from torchsummary import summary
 import numpy as np
@@ -9,16 +9,18 @@ tree_arch = [2, 4, 4, 4, 4, 4]
 mlp_dim = [256, 64, 8]
 
 class TopNet(nn.Module):
-    def __init__(self, encoder_feature_dim, device):
+    def __init__(self, encoder_feature_dim, decoder_feature_dim, npts):
         super(TopNet, self).__init__()
-        self.decoder_feature_dim = mlp_dim[-1]
+        self.decoder_feature_dim = decoder_feature_dim
+        self.tree_arch = get_arch(tree_arch, npts)
 
         # encoder
         self.pcn_encoder= PCNencoder(encoder_feature_dim)
         # decoder: root node
-        self.mlp = decoder_mlp(encoder_feature_dim, self.decoder_feature_dim, mlp_dim, tree_arch[0])
+        self.mlp = decoder_mlp(encoder_feature_dim, self.decoder_feature_dim, self.tree_arch[0])
         # decoder: leaf node
-        self.mlpconv_layers = decoder_mlp_conv(encoder_feature_dim, self.decoder_feature_dim, tree_arch, device)
+        self.mlpconv_layers = decoder_mlp_conv(encoder_feature_dim, self.decoder_feature_dim,
+                                               self.tree_arch, npts)
 
     def forward(self, x):
         feature = self.pcn_encoder(x)
@@ -29,5 +31,5 @@ class TopNet(nn.Module):
 
 
 if __name__ == '__main__':
-    model = TopNet(1024).cuda()
+    model = TopNet(1024, 8, 16384).cuda()
     summary(model, (3,2048))
