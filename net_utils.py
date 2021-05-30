b3e98905e2b8f0c1d26ae434d82bc017e732a1bb
2yejin2 <62421163+2yejin2@users.noreply.github.com>
2021-05-31 01:06:12 +0900

    Update net_utils.py

--- net_utils.py
+++ net_utils.py
@@ -3,7 +3,8 @@ import torch.nn.functional as F
 import torch
 import numpy as np
 import math
-# encoder
+
+# PCN encoder
 class PCNencoder(nn.Module):
     def __init__(self, feature_dim):
         super(PCNencoder, self).__init__()
@@ -40,6 +41,57 @@ class PCNencoder(nn.Module):
         x = torch.max(x, 2)[0]
 
         return x
+
+# PCN decoder
+class PCNdecoder(nn.Module):
+    def __init__(self, num_coarse=1024, num_dense=16384):
+        super(PCNdecoder, self).__init__()
+
+        self.num_coarse = num_coarse
+        self.num_dense = num_dense
+
+        # fully connected layers
+        self.linear1 = nn.Linear(1024, 1024)
+        self.linear2 = nn.Linear(1024, 1024)
+        self.linear3 = nn.Linear(1024, 3 * num_coarse)
+        self.bn1 = nn.BatchNorm1d(1024)
+        self.bn2 = nn.BatchNorm1d(1024)
+
+        # shared mlp
+        self.conv1 = nn.Conv1d(3 + 2 + 1024, 512, 1)
+        self.conv2 = nn.Conv1d(512, 512, 1)
+        self.conv3 = nn.Conv1d(512, 3, 1)
+        self.bn3 = nn.BatchNorm1d(512)
+        self.bn4 = nn.BatchNorm1d(512)
+
+        # 2D grid
+        grids = np.meshgrid(np.linspace(-0.05, 0.05, 4, dtype=np.float32),
+                            np.linspace(-0.05, 0.05, 4, dtype=np.float32))  # (2, 4, 44)
+        self.grids = torch.Tensor(grids).view(2, -1)  # (2, 4, 4) -> (2, 16)
+
+    def forward(self, x):
+        b = x.size()[0]
+        # global features
+        v = x  # (B, 1024)
+
+        # fully connected layers to generate the coarse output
+        x = F.relu(self.bn1(self.linear1(x)))
+        x = F.relu(self.bn2(self.linear2(x)))
+        x = self.linear3(x)
+        y_coarse = x.view(-1, 3, self.num_coarse)  # (B, 3, 1024)
+
+        repeated_centers = y_coarse.unsqueeze(3).repeat(1, 1, 1, int(self.num_dense/self.num_coarse)).view(b, 3, -1)  # (B, 3, 16x1024)
+        repeated_v = v.unsqueeze(2).repeat(1, 1, self.num_dense)  # (B, 1024, 16x1024)
+        grids = self.grids.to(x.device)  # (2, 16)
+        grids = grids.unsqueeze(0).repeat(b, 1, self.num_coarse)  # (B, 2, 16x1024)
+
+        x = torch.cat([repeated_v, grids, repeated_centers], dim=1)  # (B, 2+3+1024, 16x1024)
+        x = F.relu(self.bn3(self.conv1(x)))
+        x = F.relu(self.bn4(self.conv2(x)))
+        x = self.conv3(x)  # (B, 3, 16x1024)
+        y_detail = x + repeated_centers  # (B, 3, 16x1024)
+
+        return y_coarse, y_detail
 
 # mlp for root node of decoder
 class decoder_mlp(nn.Module):
@@ -115,11 +167,26 @@ class decoder_mlp_conv(nn.Module):
         self.conv51_d = nn.Conv1d(input_channel, int(input_channel / 2), 1)
         self.conv52_d = nn.Conv1d(int(input_channel / 2), int(input_channel / 4), 1)
         self.conv53_d = nn.Conv1d(int(input_channel / 4), int(input_channel / 8), 1)
-        self.conv54_d = nn.Conv1d(int(input_channel / 8), 3 * tree_arch[5], 1)
+        self.conv54_d = nn.Conv1d(int(input_channel / 8), output_channel * tree_arch[5], 1)
         self.bn51_d = nn.BatchNorm1d(int(input_channel / 2))
         self.bn52_d = nn.BatchNorm1d(int(input_channel / 4))
         self.bn53_d = nn.BatchNorm1d(int(input_channel / 8))
 
+        self.conv61_d = nn.Conv1d(input_channel, int(input_channel / 2), 1)
+        self.conv62_d = nn.Conv1d(int(input_channel / 2), int(input_channel / 4), 1)
+        self.conv63_d = nn.Conv1d(int(input_channel / 4), int(input_channel / 8), 1)
+        self.conv64_d = nn.Conv1d(int(input_channel / 8), output_channel * tree_arch[6], 1)
+        self.bn61_d = nn.BatchNorm1d(int(input_channel / 2))
+        self.bn62_d = nn.BatchNorm1d(int(input_channel / 4))
+        self.bn63_d = nn.BatchNorm1d(int(input_channel / 8))
+
+        self.conv71_d = nn.Conv1d(input_channel, int(input_channel / 2), 1)
+        self.conv72_d = nn.Conv1d(int(input_channel / 2), int(input_channel / 4), 1)
+        self.conv73_d = nn.Conv1d(int(input_channel / 4), int(input_channel / 8), 1)
+        self.conv74_d = nn.Conv1d(int(input_channel / 8), 3 * tree_arch[7], 1)
+        self.bn71_d = nn.BatchNorm1d(int(input_channel / 2))
+        self.bn72_d = nn.BatchNorm1d(int(input_channel / 4))
+        self.bn73_d = nn.BatchNorm1d(int(input_channel / 8))
 
     def forward(self, x, feature):
             total_node = {}
@@ -162,10 +229,28 @@ class decoder_mlp_conv(nn.Module):
             feature_expand = feature.unsqueeze(2).repeat(1, 1, total_node[3])
             x = torch.cat([x, feature_expand], dim=1)
 
-            x = F.relu(self.bn51_d(self.conv51_d(x)))
-            x = F.relu(self.bn52_d(self.conv52_d(x)))
-            x = F.relu(self.bn53_d(self.conv53_d(x)))
+            x = F.relu(self.bn51_d(self.conv41_d(x)))
+            x = F.relu(self.bn52_d(self.conv42_d(x)))
+            x = F.relu(self.bn53_d(self.conv43_d(x)))
             x = self.conv54_d(x)
+            x = x.view(-1, self.decoder_feature_dim, total_node[4])
+            x = F.tanh(x)
+            feature_expand = feature.unsqueeze(2).repeat(1, 1, total_node[4])
+            x = torch.cat([x, feature_expand], dim=1)
+
+            x = F.relu(self.bn61_d(self.conv61_d(x)))
+            x = F.relu(self.bn62_d(self.conv62_d(x)))
+            x = F.relu(self.bn63_d(self.conv63_d(x)))
+            x = self.conv64_d(x)
+            x = x.view(-1, self.decoder_feature_dim, total_node[5])
+            x = F.tanh(x)
+            feature_expand = feature.unsqueeze(2).repeat(1, 1, total_node[5])
+            x = torch.cat([x, feature_expand], dim=1)
+
+            x = F.relu(self.bn71_d(self.conv71_d(x)))
+            x = F.relu(self.bn72_d(self.conv72_d(x)))
+            x = F.relu(self.bn73_d(self.conv73_d(x)))
+            x = self.conv74_d(x)
             x = x.view(-1, 3, self.npts)
             x = F.tanh(x)
 
