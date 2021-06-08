6339c272854d09a0c8ce34574fd0dfa4ed51e6d6
2yejin2 <62421163+2yejin2@users.noreply.github.com>
2021-06-08 16:52:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,156 +1,39 @@
-import dataset
-from torch.utils.data import DataLoader
-from model import TopNet, PCN
-from loss import chamfer_distance
-import os
-import torch
-import argparse
-import torch.optim as optim
-from utils import Logger, AverageMeter, draw_curve, mix_up
-import json
 import torch.nn as nn
-from torch.optim import lr_scheduler
-import numpy as np
+from net_utils import PCNencoder, PCNdecoder, decoder_mlp, decoder_mlp_conv, get_arch
+from torchsummary import summary
 
-parser = argparse.ArgumentParser(description='Point Cloud Completion Project')
-parser.add_argument('--save_path', default='./exp', type=str,
-                    help='savepath')
-parser.add_argument('--data_path', default='./shapenet', type=str,
-                    help='datapath')
-parser.add_argument('--npts', default=16384, type=int,
-                    help='number of generated points(dense)')
-parser.add_argument('--coarse', default=1024, type=int,
-                    help='number of generated points(coarse)')
-parser.add_argument('--alpha', default=0.5, type=int,
-                    help='coarse loss weight')
-parser.add_argument('--model', default='topnet', type=str,
-                    help='topnet or pcn')
-parser.add_argument('--embedding_dim', default=1024, type=int,
-                    help='embedding size')
-parser.add_argument('--batch_size', default=64, type=int,
-                    help='batch size')
-parser.add_argument('--optim', default='adagrad', type=str,
-                    help='optimizer')
-parser.add_argument('--lr', default=0.1e-2, type=float,
-                    help='learning rate')
-parser.add_argument('--epochs', default=300, type=int,
-                    help='train epoch')
-parser.add_argument('--weight_decay', default=0.000001, type=float,
-                    help='weight_decay')
-parser.add_argument('--scaling', default=None, type=float,
-                    help='scaling size')
-parser.add_argument('--rotation', default=False, type=bool,
-                    help='If "true", randomly rotate the point')
-parser.add_argument('--mirror_prob', default=None, type=float,
-                    help='Probability of randomly mirroring points')
-parser.add_argument('--crop_prob', default=None, type=float,
-                    help='Probability of cropping')
-parser.add_argument('--mixup_prob', default=None, type=float,
-                    help='Probability of mixup')
-parser.add_argument('--emd', default=False, type=bool,
-                    help='mixup using emd')
-parser.add_argument('--gpu_id', default='1', type=str, help='devices')
-args = parser.parse_args()
+tree_arch = [2, 2, 2, 2, 2, 4, 4, 4]
+
+class TopNet(nn.Module):
+    def __init__(self, encoder_feature_dim, decoder_feature_dim, npts):
+        super(TopNet, self).__init__()
+        self.decoder_feature_dim = decoder_feature_dim
+        self.tree_arch = get_arch(tree_arch, npts)
+
+        # encoder
+        self.pcn_encoder= PCNencoder(encoder_feature_dim)
+        # decoder: root node
+        self.mlp = decoder_mlp(encoder_feature_dim, self.decoder_feature_dim, self.tree_arch[0])
+        # decoder: leaf node
+        self.mlpconv_layers = decoder_mlp_conv(encoder_feature_dim, self.decoder_feature_dim,
+                                               self.tree_arch, npts)
+
+    def forward(self, x):
+        feature = self.pcn_encoder(x)
+        x = self.mlp(feature)
+        x = self.mlpconv_layers(x, feature)
+
+        return x
 
 
-def train(model, trn_loader, criterion, optimizer, epoch, num_epoch, train_logger):
-    model.train()
-    train_loss = AverageMeter()
-    for i, (data, target, coarsegt, densegt) in enumerate(trn_loader):
-        data, target, coarsegt, densegt = data.cuda(), target.cuda(), coarsegt.cuda(), densegt.cuda()
-        if args.mixup_prob is not None and np.random.rand(1) < args.mixup_prob:
-            data, coarsegt, densegt = mix_up(data, coarsegt, densegt, 'naive', 1)
+class PCN(nn.Module):
+    def __init__(self, encoder_feature_dim, num_coarse, num_dense): # num_dense = npts
+        super(PCN, self).__init__()
 
-        output = model(data)
-        if args.model == 'topnet':
-            loss, _ = criterion(output.transpose(1,2), densegt.transpose(1,2))
-        elif args.model == 'pcn':
-            loss1, _ = criterion(output[1].transpose(1,2), coarsegt.transpose(1,2))
-            loss2, _ = criterion(output[2].transpose(1, 2), densegt.transpose(1, 2))
-            loss = args.alpha * loss1 + loss2
-        train_loss.update(loss.item()*10000)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+        self.encoder = PCNencoder(encoder_feature_dim)
+        self.decoder = PCNdecoder(num_coarse, num_dense)
 
-        if i % 10 == 0 and i != 0:
-            print('Epoch : [{0}/{1}] [{2}/{3}]  Train Loss : {loss:.4f}'.format(
-                epoch, num_epoch, i, len(trn_loader), loss=loss*10000))
-    train_logger.write([epoch, train_loss.avg])
-
-
-def test(model, tst_loader, criterion, epoch, num_epoch, val_logger):
-    model.eval()
-    val_loss = AverageMeter()
-    with torch.no_grad():
-        for i, (data, target, coarsegt, densegt) in enumerate(tst_loader):
-            data, target, coarsegt, densegt = data.cuda(), target.cuda(), coarsegt.cuda(), densegt.cuda()
-            output = model(data)
-            if args.model == 'topnet':
-                loss, _ = criterion(output.transpose(1, 2), densegt.transpose(1, 2))
-            elif args.model == 'pcn':
-                loss1, _ = criterion(output[1].transpose(1, 2), coarsegt.transpose(1, 2))
-                loss2, _ = criterion(output[2].transpose(1, 2), densegt.transpose(1, 2))
-                loss = args.alpha * loss1 + loss2
-            val_loss.update(loss.item()*10000)
-
-        print("=================== TEST(Validation) Start ====================")
-        print('Epoch : [{0}/{1}]  Test Loss : {loss:.4f}'.format(
-                epoch, num_epoch, loss=val_loss.avg))
-        print("=================== TEST(Validation) End ======================")
-        val_logger.write([epoch, val_loss.avg])
-
-
-def main():
-    save_path=args.save_path
-    if not os.path.exists(save_path):
-        os.makedirs(save_path)
-        # Save configuration
-        with open(save_path + '/configuration.json', 'w') as f:
-            json.dump(args.__dict__, f, indent=2)
-    os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu_id
-
-    # define architecture
-    if args.model == 'topnet':
-        network = TopNet(encoder_feature_dim = args.embedding_dim, decoder_feature_dim = 128, npts = args.npts).cuda()
-    else:
-        network = PCN(encoder_feature_dim = args.embedding_dim, num_coarse = args.coarse ,num_dense = args.npts).cuda()
-    network = nn.DataParallel(network).cuda()
-
-    # load dataset
-    train_dataset = dataset.ShapeNetDataset(args.data_path, mode='train', point_class = 'all',
-                                            scaling = args.scaling, rotation = args.rotation,
-                                            mirror_prob = args.mirror_prob, crop_prob = args.crop_prob,
-                                            num_coarse = args.coarse, num_dense = args.npts, emd = args.emd)
-    val_dataset = dataset.ShapeNetDataset(args.data_path, mode='val', point_class = 'all')
-
-    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=4)
-    val_loader = DataLoader(val_dataset, batch_size=1, shuffle=False, num_workers=4)
-    print(f"Data Loaded {len(train_dataset)}")
-
-    # define criterion
-    criterion = chamfer_distance().cuda()
-    if args.optim == 'sgd':
-        optimizer = optim.SGD(network.parameters(), lr=args.lr, weight_decay=args.weight_decay)
-    elif args.optim == 'adam':
-        optimizer = optim.Adam(network.parameters(), lr=args.lr, weight_decay=args.weight_decay)
-    elif args.optim == 'adagrad':
-        optimizer = optim.Adagrad(network.parameters(), lr=args.lr, weight_decay=args.weight_decay)
-    scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[100, 150], gamma=0.7)
-
-    # logger
-    train_logger = Logger(os.path.join(save_path, 'train_loss.log'))
-    val_logger = Logger(os.path.join(save_path, 'val_loss.log'))
-
-    # training & validation
-    for epoch in range(1, args.epochs+1):
-        train(network, train_loader, criterion ,optimizer, epoch, args.epochs, train_logger)
-        test(network, val_loader, criterion, epoch, args.epochs, val_logger)
-        scheduler.step()
-        if epoch%20 == 0 or epoch == args.epochs :
-            torch.save(network.state_dict(), '{0}/{1}_{2}.pth'.format(save_path, args.model ,epoch))
-    draw_curve(save_path, train_logger, val_logger)
-    print("Process complete")
-
-if __name__ == '__main__':
-    main()
+    def forward(self, x):
+        v = self.encoder(x)
+        y_coarse, y_detail = self.decoder(v)
+        return v, y_coarse, y_detail
